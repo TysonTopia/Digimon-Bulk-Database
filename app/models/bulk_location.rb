dd69fd704b52a513d52c5771433072374f78a85e
Tyson Hartley <111388363+TysonTopia@users.noreply.github.com>
2026-07-16 14:14:05 -0600

    In Progerss

--- app/models/bulk_location.rb
+++ app/models/bulk_location.rb
@@ -1,3 +1,6 @@
 class BulkLocation < ApplicationRecord
   self.table_name = "bulk_location"
+  belongs_to :card_information,
+             foreign_key: :card_id,
+             primary_key: :card_id
 end
