8a5b20d9268725f74a5b3c5e84a85e6882fbdf46
Tyson Hartley <111388363+TysonTopia@users.noreply.github.com>
2026-07-16 13:20:25 -0600

    Search Bar Added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/articles", to: "articles#index"
   get "/bulk", to: "bulk#index"
+  get "/search", to: "bulk#index"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
